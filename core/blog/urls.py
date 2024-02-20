2ecae01921dad7cbb8f12aa9bce0ddfec7816137
kamalix <kamal.amani1994@gmail.com>
2024-02-20 16:12:48 +0330

    deeper in RedirectView
    +
    https://docs.djangoproject.com/en/3.2/ref/class-based-views/base/#django.views.generic.base.RedirectView

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -9,8 +9,6 @@ app_name = "blog"
 
 
 urlpatterns = [
-    path('fbv-index', views.IndexView,name='fbv-index'),
-    #path('cbv-index/', TemplateView.as_view(template_name="index.html",extra_context = {'name':'ali'})),
     path('cbv-index/',views.IndexView.as_view(),name='cbv-index'),
-    path('go-to-index/', RedirectView.as_view(pattern_name='blog:cbv-index'), name='redirect-to-maktabkhooneh'),
+    path('go-to-maktabkhooneh/<int:pk>/', views.RedirectToMaktab.as_view(), name='redirect-to-maktabkhooneh'),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.generic.base import TemplateView,RedirectView
 from .models import Post
 
 
 # Create your views here.
-
+'''
+#Function based view show a template
 def IndexView(request):
-    '''
-    a function based view to show index page
-    '''
-    context={'name':'ali'}
-    return render(request,"index.html",context)
+     """
+     a function based view to show index page
+     """
+     context={'name':'ali'}
+     return render(request,"index.html",context)
+'''
+
 
 class IndexView(TemplateView):
     '''
@@ -23,3 +26,28 @@ class IndexView(TemplateView):
         context['name'] = 'ali'
         context['posts'] = Post.objects.all()
         return context
+
+'''FBV for redirect   
+def RedirectToMaktab(request):
+    return redirect("https://www.maktabkhooneh.com")
+'''
+
+
+class RedirectToMaktab(RedirectView):
+    url = 'https://www.maktabkhooneh.com'
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+
+
+
+
+
+
+
+
+
+
+
+
